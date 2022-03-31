a789cb6d48e51a142bdc288501ca7c8bda3b2075
leemin-hyeong <lmhstrar070@naver.com>
2022-04-01 03:41:26 +0900

    baekjoon

---
+++ baekjoon1011.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class baekjoon1011{
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     StringBuilder sb=new StringBuilder();
+
+    //     int testCase=sc.nextInt();
+    //     for(int i=0; i<testCase; i++){
+    //         int start=sc.nextInt(), end=sc.nextInt();
+    //         int distance=end-start, move=1, count=0;
+    //         while(distance!=0){
+    //             int cal=0;
+    //             distance-=move;
+    //             count++;
+    //             cal=(move+1)*(move+2)/2;
+    //             if(distance-cal>=0)
+    //                 move++;
+    //             else if(distance-cal+move+1<0)
+    //                 move--;
+    //         }
+    //         sb.append(count).append("\n");
+    //     }
+    //     sc.close();
+    //     System.out.println(sb);
+    // }
+    public static void main(String[] args) {
+        
+    }
+    
+}
