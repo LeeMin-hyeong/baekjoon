b996a27f366cc62d9b3144c37dfb9055b1b57294
LeeMin-hyeong <lmhstrar070@naver.com>
2023-10-29 19:48:18 +0900

    ps

--- boj11780.cpp
+++ boj11780.cpp
@@ -18,7 +18,20 @@ void floyd(){
     }
 }
 
+void path(int i, int j){
+    if(p[i][j] == 0){
+        v.push_back(i);
+        v.push_back(j);
+        return;
+    }
+    path(i, p[i][j]);
+    v.pop_back();
+    path(p[i][j], j);
+}
+
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> n >> m;
     memset(d, 0x3f, sizeof(d));
     for(int i=1; i<=n; i++){
@@ -28,20 +41,29 @@ int main(){
         int u, v, w; cin >> u >> v >> w;
         if(d[u][v] > w){
             d[u][v] = w;
-            p[u][v] = v;
         }
     }
     floyd();
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            if(d[i][j]>1000000000) cout << 0 << ' ';
+            if(d[i][j] > 1000000000) cout << 0 << ' ';
             else cout << d[i][j] << ' ';
         }
         cout << '\n';
     }
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            
+            v.clear();
+            if(d[i][j] > 1000000000 || d[i][j] == 0){
+                cout << "0\n";
+                continue;
+            }
+            path(i, j);
+            cout << v.size() << ' ';
+            for(int i=0; i<v.size(); i++){
+                cout << v[i] << ' ';
+            }
+            cout << '\n';
         }
     }
 }
