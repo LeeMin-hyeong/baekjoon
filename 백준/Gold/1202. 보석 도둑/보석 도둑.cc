951df153592bf82297a2614a68797ea2d2593bd7
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-02-12 23:26:08 +0900

    [Gold II] Title: 보석 도둑, Time: 468 ms, Memory: 12272 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+long long ans;
+typedef pair<int, int> gem;
+priority_queue<gem> gems;
+priority_queue<int> bag;
+
+int main(){
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        int m, v;
+        cin >> m >> v;
+        gems.push(gem(-m, v));
+    }
+    for(int i=0; i<k; i++){
+        int c;
+        cin >> c;
+        bag.push(-c);
+    }
+    priority_queue<int> candidate;
+    while(!bag.empty()){
+        int c = -bag.top();
+        bag.pop();
+
+        while(!gems.empty() && -gems.top().first <= c){
+            candidate.push(gems.top().second);
+            gems.pop();
+        }
+
+        if(!candidate.empty()){
+            ans += candidate.top();
+            candidate.pop();
+        }
+    }
+    cout << ans << '\n';
+}
