392fa267c9ee2409bf3236721e42506a46197fab
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-02-12 23:26:37 +0900

    [Gold II] Title: 보석 도둑, Time: 192 ms, Memory: 12388 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -8,6 +8,8 @@ priority_queue<gem> gems;
 priority_queue<int> bag;
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n >> k;
     for(int i=0; i<n; i++){
         int m, v;
