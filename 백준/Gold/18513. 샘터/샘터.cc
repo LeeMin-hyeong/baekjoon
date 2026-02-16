7391a5815be69997320a67546b411444ec0ed7e1
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-02-17 02:38:56 +0900

    [Gold IV] Title: 샘터, Time: 96 ms, Memory: 12556 KB -BaekjoonHub

---
+++ 백준/Gold/18513. 샘터/샘터.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+unordered_map<int, bool> o;
+queue<pair<int, int>> q;
+
+int main(){
+    cin >> n >> k;
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        q.push({a, 0});
+        o[a] = true;
+    }
+    int cnt = 0;
+    long long ans = 0;
+    while(cnt < k){
+        int loc = q.front().first;
+        int dist = q.front().second;
+        q.pop();
+        if(!o[loc-1]){
+            q.push({loc-1, dist+1});
+            o[loc-1] = true;
+            cnt++;
+            ans += dist+1;
+        }
+        if(cnt == k) break;
+        if(!o[loc+1]){
+            q.push({loc+1, dist+1});
+            o[loc+1] = true;
+            cnt++;
+            ans += dist+1;
+        }
+    }
+    cout << ans << '\n';
+}
