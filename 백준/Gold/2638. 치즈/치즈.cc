f60bac7ef2ddb24ab7b4811069b229057927386e
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-03-04 11:12:11 +0900

    [Gold III] Title: 치즈, Time: 16 ms, Memory: 2244 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.cc
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -5,42 +5,14 @@ using namespace std;
 
 int n, m;
 int cheese[100][100];
-bool air[100][100];
 bool visited[100][100];
+int contact[100][100];
 int dy[] = {0, 1, 0, -1};
 int dx[] = {1, 0, -1, 0};
 queue<pair<int, int>> edges;
 
-void bfs(int i, int j){
-    visited[i][j] = true;
-    queue<pair<int, int>> q;
-    q.push({i, j});
-    while(!q.empty()){
-        int y = q.front().first;
-        int x = q.front().second;
-        q.pop();
-        int cnt = 0;
-        for(int k=0; k<4; k++){
-            int ny = y+dy[k];
-            int nx = x+dx[k];
-            if(ny >= 0 && ny < n && nx >= 0 && nx < m){
-                if(!visited[ny][nx] && cheese[ny][nx] == 1){
-                    visited[ny][nx] = true;
-                    q.push({ny, nx});
-                }
-                if(cheese[ny][nx] == 0 && air[ny][nx]){
-                    cnt++;
-                }
-            }
-        }
-        if(cnt >= 2){
-            edges.push({y, x});
-        }
-    }
-}
-
-void check_air(){
-    air[0][0] = true;
+bool check_air(){
+    visited[0][0] = true;
     queue<pair<int, int>> q;
     q.push({0, 0});
     while(!q.empty()){
@@ -51,13 +23,21 @@ void check_air(){
             int ny = y+dy[k];
             int nx = x+dx[k];
             if(ny >= 0 && ny < n && nx >= 0 && nx < m){
-                if(!air[ny][nx] && cheese[ny][nx] == 0){
-                    air[ny][nx] = true;
+                if(!visited[ny][nx] && cheese[ny][nx] == 0){
+                    visited[ny][nx] = true;
                     q.push({ny, nx});
+                }
+                if(cheese[ny][nx] == 1){
+                    contact[ny][nx] += 1;
+                    if(contact[ny][nx] >= 2 && !visited[ny][nx]){
+                        edges.push({ny, nx});
+                        visited[ny][nx] = true;
+                    }
                 }
             }
         }
     }
+    return edges.empty();
 }
 
 int main(){
@@ -71,19 +51,10 @@ int main(){
     }
     int ans = 0;
     while(true){
-        memset(air, false, sizeof(air));
         memset(visited, false, sizeof(visited));
+        memset(contact, 0, sizeof(contact));
         check_air();
-        bool flag = true;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(!visited[i][j] && cheese[i][j] == 1){
-                    flag = false;
-                    bfs(i, j);
-                }
-            }
-        }
-        if(flag) break;
+        if(check_air()) break;
         while(!edges.empty()){
             cheese[edges.front().first][edges.front().second] = 0;
             edges.pop();
