a87b672dd1105bc9c4128e27c3bce94ee419ae3b
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-04-05 00:01:28 +0900

    [Silver II] Title: 베르트랑 공준, Time: 4 ms, Memory: 3240 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+bool prime[250000];
+int ans[250000];
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    memset(prime, true, sizeof(prime));
+    prime[0] = false;
+    prime[1] = false;
+    for(int i=2; i<250000; i++){
+        if(prime[i]){
+            for(int j=2; i*j<250000; j++){
+                prime[i*j] = false;
+            }
+        }
+    }
+    // for(int i=1; i<123457;)
+    while(true){
+        int n; cin >> n;
+        if(n == 0) break;
+        int cnt = 0;
+        for(int i=n+1; i<=2*n; i++){
+            if(prime[i]) cnt++;
+        }
+        cout << cnt << '\n';
+    }
+}
