d9f3d24ea469ad389f776cf11941e63fd487e256
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-04-09 03:21:31 +0900

    [Gold IV] Title: 세 수의 합, Time: 136 ms, Memory: 23444 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <set>
 using namespace std;
 
 int n;
-vector<int> u, sum;
+vector<int> u;
+unordered_map<int, bool> m;
+set<int> s;
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -18,15 +22,16 @@ int main(){
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            sum.push_back(u[i] + u[j]);
+            m[u[i] + u[j]] = true;
+            // s.insert(u[i] + u[j]);
         }
     }
-    sort(sum.begin(), sum.end());
 
     for(int i=n-1; i>=0; i--){
         for(int j=0; j<n; j++){
             int target = u[i] - u[j];
-            if(binary_search(sum.begin(), sum.end(), target)){
+            if(m[target]){
+            // if(s.find(target) != s.end()){
                 cout << u[i] << '\n';
                 return 0;
             }
