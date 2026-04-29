83fa2967ed24bd4bfd820b9de8314f676818f85c
MinhyeongLee <41287145+LeeMin-hyeong@users.noreply.github.com>
2026-04-29 17:13:44 +0900

    [level 2] Title: 올바른 괄호, Time: 0.54 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<char> st;
+
+bool solution(string s)
+{
+    bool answer = true;
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '('){
+            st.push('(');
+        }
+        else{
+            if(!st.empty() && st.top() == '('){
+                st.pop();
+            }
+            else{
+                answer = false;
+            }
+        }
+    }
+    if(!st.empty()) answer = false;
+    
+
+    return answer;
+}
